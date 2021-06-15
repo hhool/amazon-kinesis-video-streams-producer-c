dbee7575472036d7e036611d18f29353f099509f
hhool <hhool.student@gmail.com>
2021-06-15 17:50:28 +0800

    chore: modify for windows build

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,6 +1,5 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
-cmd.exe /c cmake -G "NMake Makefiles" ..
-cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE ..
+cmake -G "NMake Makefiles" -DBUILD_TEST=OFF -DBUILD_SAMPLE=OFF -DBUILD_STATIC=ON -DOPEN_SRC_INSTALL_PREFIX="E:\project\w53\aws\aws-akvs-install" ..
 nmake
